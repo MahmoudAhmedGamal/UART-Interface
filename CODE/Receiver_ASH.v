db048db1975cb87b023658922f22ac24f5c1f365
MahmoudAhmedGamal <7odagamal2004@gmail.com>
2025-07-27 14:01:17 +0300

    Update Receiver_ASH.v
    FSM appropriate model

--- CODE/Receiver_ASH.v
+++ CODE/Receiver_ASH.v
@@ -12,7 +12,7 @@ module Receiver_ASH(
 
     reg stop;
     reg [2:0] bit_index;
-    reg [3:0] state;//, state <
+    reg [3:0] state, state_next;
     reg [7:0] data_reg; 
     reg parity_error_reg, stop_error_reg; // Add registers
     localparam IDLE = 3'b000,
@@ -32,12 +32,11 @@ module Receiver_ASH(
             Valid_rx_reg <= 0;
         end
     else begin
-        //state <= state;
+        state <= state_next;
         case (state)
             IDLE: begin
                 Valid_rx_reg <= 0;
                 if (!RXD)begin
-                    state <= START;
                     sample_counter <= 0;
                     bit_index <= 0;
 
@@ -47,7 +46,6 @@ module Receiver_ASH(
             START: begin 
                         if (sample_counter == 7)begin  // Sample at middle of start bit
                             sample_counter <= 0;
-                            state <= DATA;  
                         end                  
                         else begin 
                             sample_counter <= sample_counter + 1;
@@ -58,7 +56,6 @@ module Receiver_ASH(
                             data_reg[bit_index] <= RXD;
                             sample_counter <= 0;
                             if(bit_index == 7)begin
-                                state <= PARITY; // Move to parity bit after data bits
                                 parity_bit <= ^data_reg;
                             end
                             else begin
@@ -71,7 +68,6 @@ module Receiver_ASH(
             PARITY: begin
                     if (sample_counter == 15)begin  // Sample at middle of data bit
                         sample_counter <= 0;
-                        state <= STOP;
                         if(parity_bit = RXD)begin
                         parity_error_reg <=0 ;
                         end
@@ -93,21 +89,73 @@ module Receiver_ASH(
                         else begin
                             stop_error_reg <= 1;
                         end
-                        state <= IDLE; // Go back to idle state
                     end
                     else begin
                         sample_counter <= sample_counter + 1;
                     end
 
             end
-            default: state <= IDLE; // Fallback to idle on unknown state
         endcase
     end
     end
+
+    always @(*)begin
+        case (state)
+            state_next = state;
+            IDLE: begin
+                if (!RXD)begin
+                    state_next = START;
+                end
+                else begin
+                    state_next = IDLE;
+                end
+                
+            end
+            START: begin 
+                        if (sample_counter == 7)begin  // Sample at middle of start bit
+                            state_next = DATA;  
+                        end   
+                            state_next = START;  
+                        else begin 
+                        end                   
+            end            
+            DATA: begin
+                    if (sample_counter == 15) begin  // Sample at middle of data bit
+                            if(bit_index == 7)begin
+                                state_next = PARITY; // Move to parity bit after data bits
+                            end
+                            else begin
+                            state_next = DATA;  
+
+                            end
+                    end else begin
+                        state_next = DATA;  
+                    end
+            end
+            PARITY: begin
+                    if (sample_counter == 15)begin  // Sample at middle of data bit
+                        state_next = STOP;
+                    end
+                    else begin
+                        state_next = PARITY;                   
+                    end
+            end
+            STOP: begin
+                    if (sample_counter == 15)begin  // Sample stop bit
+                        state_next = IDLE; // Go back to idle state
+                    end
+                    else begin
+                        state_next = STOP;
+                    end
+
+            end
+            default: state_next = IDLE; // Fallback to idle on unknown state
+        endcase
+    end
     
     assign RX_Data = (state == STOP)? data_reg : 0;
-    assign Parity_error = parity_error_reg;
-    assign Stop_error = stop_error_reg;
-    assign Valid_rx = Valid_rx_reg;
+    assign Parity_error =(state == STOP)? parity_error_reg : 0;
+    assign Stop_error = (state == STOP)? stop_error_reg : 0;
+    assign Valid_rx = (state == STOP )? Valid_rx_reg;
 
 endmodule
